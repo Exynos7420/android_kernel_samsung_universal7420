209376ae3c79f9073b6d1ca63fa472d4a2652007
LuK1337 <priv.luk@gmail.com>
2017-02-27 13:49:33 +0100

    input: touchscreen: fts: Add FB notifier callback
    Change-Id: Ia9b742a5fa83b0d9159706faa65bfd2cb6db78eb

--- drivers/input/touchscreen/stm/fts_ts.c
+++ drivers/input/touchscreen/stm/fts_ts.c
@@ -73,6 +73,11 @@
 #include <linux/input/mt.h>
 #include "fts_ts.h"
 
+#ifdef CONFIG_FB
+#include <linux/notifier.h>
+#include <linux/fb.h>
+#endif
+
 #ifdef CONFIG_TOUCHSCREEN_DOUBLETAP2WAKE
 #include <linux/input/doubletap2wake.h>
 #else
@@ -1892,6 +1897,12 @@ static int fts_setup_drv_data(struct i2c_client *client)
 #ifdef CONFIG_BATTERY_SAMSUNG
 extern unsigned int lpcharge;
 #endif
+
+#ifdef CONFIG_FB
+static int fb_notifier_callback(struct notifier_block *self,
+	unsigned long event, void *data);
+#endif
+
 static int fts_probe(struct i2c_client *client, const struct i2c_device_id *idp)
 {
 	int retval;
@@ -2135,6 +2146,12 @@ static int fts_probe(struct i2c_client *client, const struct i2c_device_id *idp)
 #endif
 #endif
 
+#ifdef CONFIG_FB
+	info->fb_notif.notifier_call = fb_notifier_callback;
+	if (fb_register_client(&info->fb_notif))
+		pr_err("%s: could not create fb notifier\n", __func__);
+#endif
+
 	device_init_wakeup(&client->dev, true);
 
 	return 0;
@@ -2214,6 +2231,10 @@ static int fts_remove(struct i2c_client *client)
 	info->input_dev = NULL;
 
 	info->board->power(info, false);
+
+#ifdef CONFIG_FB
+	fb_unregister_client(&info->fb_notif);
+#endif
 
 	kfree(info);
 
@@ -2784,6 +2805,35 @@ static void fts_shutdown(struct i2c_client *client)
 		 info->board->led_power(info, false);
 #endif
 }
+
+#ifdef CONFIG_FB
+static int fb_notifier_callback(struct notifier_block *self,
+				unsigned long event, void *data)
+{
+	struct fb_event *evdata = data;
+	struct fts_ts_info *tc_data = container_of(self, struct fts_ts_info, fb_notif);
+
+	if (evdata && evdata->data && event == FB_EVENT_BLANK) {
+		int *blank = evdata->data;
+		switch (*blank) {
+		case FB_BLANK_UNBLANK:
+		case FB_BLANK_NORMAL:
+		case FB_BLANK_VSYNC_SUSPEND:
+		case FB_BLANK_HSYNC_SUSPEND:
+		        fts_input_open(tc_data->input_dev);
+			break;
+		case FB_BLANK_POWERDOWN:
+		        fts_input_close(tc_data->input_dev);
+			break;
+		default:
+			/* Don't handle what we don't understand */
+			break;
+		}
+	}
+
+	return 0;
+}
+#endif
 
 void fts_recovery_cx(struct fts_ts_info *info)
 {
