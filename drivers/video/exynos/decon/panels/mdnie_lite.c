TeamNexus/android_kernel_samsung_exynos7420

decon: mdnie: Store sensorRGB values in main table
The fb notifier callback will call mdnie_update() on
FB_BLANK_UNBLANK, which will then call mdnie_update_sequence()
with the table returned by mdnie_find_table(mdnie).

This will discard the values written to sensorRGB if we do not
save them.

Change-Id: I29cae3f98f64885abd3ebabc43ad13a959a3fffa

drivers/video/exynos/decon/panels/mdnie_lite.c
@@ -600,6 +600,10 @@ static ssize_t sensorRGB_store(struct device *dev,
 		mdnie->white_b = white_blue;
 
 		mdnie_update_sequence(mdnie, &(mdnie->table_buffer));
+
+		table->seq[scr_info->index].cmd[scr_info->white_r] = (unsigned char)white_red;
+		table->seq[scr_info->index].cmd[scr_info->white_g] = (unsigned char)white_green;
+		table->seq[scr_info->index].cmd[scr_info->white_b] = (unsigned char)white_blue;
 	}
 
 	return count;

Exynos7420/android_kernel_samsung_universal7420

drivers/video/exynos/decon/panels/mdnie_lite.c
@@ -482,6 +482,10 @@ static ssize_t sensorRGB_store(struct device *dev,
 		mdnie->table_buffer.seq[scr_info->index].cmd[scr_info->wb] = mdnie->wrgb_current.b = (unsigned char)white_b;
 
 		mdnie_update_sequence(mdnie, &mdnie->table_buffer);
+
+		table->seq[scr_info->index].cmd[scr_info->white_r] = (unsigned char)white_r;
+		table->seq[scr_info->index].cmd[scr_info->white_g] = (unsigned char)white_g;
+		table->seq[scr_info->index].cmd[scr_info->white_b] = (unsigned char)white_b;
 	}
 
 	return count;